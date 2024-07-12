2b6062ba4f313bc933fa77367d0c822d9ed39b7b
Ariya Panna <ariyapanna@outlook.com>
2024-07-13 03:13:33 +0700

    merging servo motor and weight sensor code

--- feeding-machine/feeding-machine.ino
+++ feeding-machine/feeding-machine.ino
@@ -1,5 +1,12 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
+#include "HX711.h"
+#include <ESP32Servo.h>
+
+// DEFINE PIN
+#define DOUT 14
+#define CLK  12
+#define SERVO_PIN 13
 
 // WiFi credentials
 const char* ssid = "cahaya majun";
@@ -13,6 +20,14 @@ const char* broker_password = "asustufgaming";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+HX711 scale;
+Servo servoController;
+
+float calibration_factor = 97.50;
+int GRAM;
+
+// GLOBAL VARIABLE
+int targetWeight = 0;
 
 // Function declarations
 void setup_wifi();
@@ -24,31 +39,59 @@ void setup()
   // Serial monitor
   Serial.begin(115200);
 
+  // Scale initialization
+  scale.begin(DOUT, CLK);
+  scale.set_scale();
+  scale.tare();
+  long zero_factor = scale.read_average();
+  Serial.print("Zero factor: ");
+  Serial.println(zero_factor);
+
+  // Servo initialization
+  servoController.attach(SERVO_PIN);
+
   // Connect to WiFi
   setup_wifi();
 
   // Set MQTT server and callback function
   client.setServer(mqtt_server, mqtt_port);
   client.setCallback(callback);
+  delay(1000);
+}
+
+void printWeight()
+{
+  scale.set_scale(calibration_factor);
+  GRAM = scale.get_units(), 4;
+  Serial.print("Reading: ");
+  Serial.print(GRAM);
+  Serial.print(" Gram");
+  Serial.print(" calibration_factor: ");
+  Serial.print(calibration_factor);
+  Serial.println();
 }
 
 void loop() 
 {
+  printWeight();
+
   // Jika koneksi MQTT terputus, coba untuk tersambung kembali
   if (!client.connected())
     reconnect();
 
   // Menghandle pesan dari broker MQTT
   client.loop();
+
+  openTube();
   
-  // Contoh pengiriman data ke broker MQTT setiap 5 detik
+  // Contoh pengiriman data ke broker MQTT setiap 3 detik
   static unsigned long lastMillis = 0;
-  if (millis() - lastMillis > 5000) 
+  if (millis() - lastMillis > 3000) 
   {
     lastMillis = millis();
     // Kirim pesan ke topik "esp32/test"
-    String message = "Hello from ESP32";
-    client.publish("esp32/test", message.c_str());
+    String message = String(GRAM);
+    client.publish("weightData", message.c_str());
   }
 }
 
@@ -74,16 +117,18 @@ void setup_wifi()
 
 void callback(char* topic, byte* payload, unsigned int length) 
 {
-  String state = "";
+  String data = "";
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.print("] ");
   
   // Print pesan yang diterima
   for (int i = 0; i < length; i++) {
-    state += (char)payload[i];
+    data += (char)payload[i];
     Serial.print((char)payload[i]);
   }
+
+  targetWeight = GRAM - data.toInt();
 
   Serial.println();
 }
@@ -96,6 +141,7 @@ void reconnect() {
     
     // ID klien unik
     String clientId = "ESP32Client-";
+
     clientId += String(random(0xffff), HEX);
     
     // Coba untuk tersambung
@@ -103,7 +149,7 @@ void reconnect() {
     {
       Serial.println("connected");
       // Subscribe ke topik "esp32/test"
-      client.subscribe("garudahacks");
+      client.subscribe("garudahacks/tes");
     } else 
     {
       Serial.print("failed, rc=");
@@ -111,6 +157,22 @@ void reconnect() {
       Serial.println(" try again in 5 seconds");
       // Tunggu 5 detik sebelum mencoba lagi
       delay(5000);
+    }
+  }
+}
+
+void openTube()
+{
+  if(targetWeight != 0)
+  {
+    if(GRAM > targetWeight)
+    {
+      servoController.write(120);
+    }
+    else
+    {
+      servoController.write(72);
+      targetWeight = 0;
     }
   }
 }
